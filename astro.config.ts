4164449f7d93af3aa0b8a6755d9c3260def164f3
Vito Esposito <info@vitoesposito.it>
2026-01-07 16:40:12 +0100

    refactor: remove trailingSlash configuration in Astro and update nginx routing to accommodate both files and directories

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   site: 'https://blog.vitoesposito.it',
-  trailingSlash: 'never',
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
