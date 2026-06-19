8cc67d0906bf1f105f93305a0eac45271a26b9bc
Vito Esposito <vesposito@wheretech.it>
2026-06-19 18:25:57 +0200

    feat(privacy policy): aggiunta pagina di privacy policy e fix minori su metadata e stile

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,26 @@
+import type { APIRoute } from 'astro'
+
+const getRobotsTxt = (sitemapURL: URL) => `User-agent: *
+Allow: /
+
+Sitemap: ${sitemapURL.href}
+`
+
+export const GET: APIRoute = ({ site }) => {
+  if (!site) {
+    return new Response('User-agent: *\nDisallow: /', {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+      },
+    })
+  }
+
+  const sitemapURL = new URL('sitemap-index.xml', site)
+  return new Response(getRobotsTxt(sitemapURL), {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+    },
+  })
+}
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,16 @@
+import type { APIContext } from 'astro';
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 import { SITE } from '../consts';
 
-export async function GET(context) {
-	const posts = await getCollection('blog');
+export async function GET(context: APIContext) {
+	const posts = (await getCollection('blog'))
+		.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+
 	return rss({
 		title: SITE.title,
 		description: SITE.description,
-		site: context.site,
+		site: context.site!,
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.id}/`,
